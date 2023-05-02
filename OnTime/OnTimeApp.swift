fb7c71d7bfb01e0d3f48b9723d6d46b80574fec3
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-05-02 22:48:36 +0400

    Initial Commit

---
+++ OnTime/OnTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTimeApp.swift
+//  OnTime
+//
+//  Created by aplle on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct OnTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
