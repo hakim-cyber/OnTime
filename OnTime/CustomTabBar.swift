f17929d277002645c6abbca791e794a16b305cd2
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-05-06 13:18:23 +0400

    start Side Bar

--- OnTime/CustomTabBar.swift
+++ OnTime/CustomTabBar.swift
@@ -68,3 +68,67 @@ struct CustomTabBar_Previews: PreviewProvider {
         CustomTabBar(selectedTab: .constant(Tab.house))
     }
 }
+
+struct CustomSideBar: View {
+    let startX:CGFloat
+    @Binding var selectedTab:Tab
+    @Binding var currentDragOffset:CGFloat
+    @Binding var endX:CGFloat
+    private var fillImage:String{
+        selectedTab.rawValue + ".fill"
+    }
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                
+                VStack{
+                    ForEach(Tab.allCases,id:\.rawValue){tab in
+                        Spacer()
+                        ZStack{
+                            Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                                .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                                .foregroundColor(selectedTab == tab ? Color.backgroundColor : .gray)
+                                .font(.system(size: 20))
+                                .offset(y: selectedTab == tab ? -10 : 0)
+                                .shadow(color: Color.green,radius:selectedTab == tab ? 15 : 0)
+                                .onTapGesture{
+                                    withAnimation(.easeIn(duration: 0.2)){
+                                        selectedTab = tab
+                                    }
+                                }
+                            if selectedTab == tab{
+                                Rectangle()
+                                    .fill(Color.green)
+                                    .frame(width: 20,height: 1.5)
+                                    .offset(y:15)
+                                
+                                
+                                
+                            }
+                            
+                        }
+                        Spacer()
+                        
+                    }
+                    
+                }
+                .frame(width: 80,height: 300)
+                .background(.ultraThinMaterial)
+                .cornerRadius(20)
+                .padding(.vertical,10)
+                .shadow(color:.green,radius: 5)
+                
+            }
+            RoundedRectangle(cornerRadius: 5)
+                            .fill(.ultraThinMaterial)
+                               .frame(width: 15,height:80)
+                               .offset(x:50)
+                          
+        }
+        .ignoresSafeArea()
+        .padding(.leading,5)
+        .frame(minWidth: .infinity, alignment: .leading)
+    }
+}
+
